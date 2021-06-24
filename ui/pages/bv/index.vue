ff8f66b6b58a96b7fc78d83dc2167ef827da0307
doer <499361328@qq.com>
2021-06-24 18:21:51 +0800

    新增BootstrapVue和自定义组件测试页面

---
+++ ui/pages/bv/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <!--  <div class="container my-class">BV</div>-->
+  <div class="main-window">
+    <div class="main-window__aside">aside</div>
+    <div class="main-window__content">content</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Bv',
+}
+</script>
+
+<style scoped>
+.main-window {
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+}
+.main-window__aside {
+  background: gray;
+  width: 20%;
+}
+.main-window__content {
+  background: black;
+  width: 80%;
+}
+.my-class {
+  width: 100%;
+  max-width: 100%;
+  border: 1px solid black;
+  background: gray;
+  height: 100vh;
+}
+
+@media (max-width: 1400px) {
+  .my-class {
+    width: 90%;
+  }
+}
+
+@media (max-width: 1200px) {
+  .my-class {
+    width: 80%;
+  }
+}
+
+@media (max-width: 992px) {
+  .my-class {
+    width: 70%;
+  }
+}
+
+@media (max-width: 768px) {
+  .my-class {
+    width: 60%;
+  }
+}
+
+@media (max-width: 576px) {
+  .my-class {
+    width: 50%;
+  }
+}
+</style>
